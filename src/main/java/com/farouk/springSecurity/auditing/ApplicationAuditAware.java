fb24c7cc96a8e6dea791fbfe701f12a689843cd0
Faroukdouh <faroukdouh06@gmail.com>
2024-11-21 08:12:41 +0100

    jpa_auditing

---
+++ src/main/java/com/farouk/springSecurity/auditing/ApplicationAuditAware.java
@@ -0,0 +1,31 @@
+package com.farouk.springSecurity.auditing;
+
+
+import com.farouk.springSecurity.user.User;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component(value = "auditorAware")
+public class ApplicationAuditAware implements AuditorAware<Integer> {
+    @Override
+    public Optional<Integer> getCurrentAuditor() {
+        Authentication authentication =
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication();
+        if (authentication == null ||
+                !authentication.isAuthenticated() ||
+                authentication instanceof AnonymousAuthenticationToken
+        ) {
+            return Optional.empty();
+        }
+
+        User userPrincipal = (User) authentication.getPrincipal();
+        return Optional.ofNullable(userPrincipal.getId());
+    }
+}
